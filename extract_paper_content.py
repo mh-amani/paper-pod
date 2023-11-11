1110301e10e140fe8dc1d6139368383167e0d0f3
nifleisch <62500421+nifleisch@users.noreply.github.com>
2023-11-11 15:13:06 +0100

    Fix newline issue in extract_paper_content function

--- extract_paper_content.py
+++ extract_paper_content.py
@@ -5,4 +5,4 @@ def extract_paper_content(url):
     loader = PyPDFLoader(url)
     documents = loader.load_and_split()
     raw_text = "\n\n".join([document.page_content for document in documents])
-    
+    return raw_text
