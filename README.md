7746d4b02a89feffbe1d3a22ee691ae5d7214e44
nifleisch <62500421+nifleisch@users.noreply.github.com>
2023-11-11 13:37:30 +0100

    Add gitignore and paper_pod.py script, and update README with installation instructions

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # paper-pod
+
+## Installation
+
+```bash
+# create virtual environment with conda
+python -m venv venv
+
+# activate virtual environment
+source venv/bin/activate
+
+# install dependencies
+pip install -r requirements.txt
+```
---
+++ paper_pod.py
@@ -0,0 +1,64 @@
+"""
+Bot that download, reads, and summarizes research papers.
+
+```python
+python paper_pod.py
+```
+
+"""
+
+import logging
+
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from keys import TELEGRAM_KEY
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+async def process_paper(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Process paper."""
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TELEGRAM_KEY).build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, process_paper))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
