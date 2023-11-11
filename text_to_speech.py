6d34f2252b4eb8c234c72f19391563abadbdf3d2
nifleisch <62500421+nifleisch@users.noreply.github.com>
2023-11-11 14:38:43 +0100

    Add text-to-speech functionality using OpenAI

---
+++ text_to_speech.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+from pathlib import Path
+
+
+def text_to_speech(text):
+    client = OpenAI()
+
+    speech_file_path = Path(__file__).parent / "speech.mp3"
+    response = client.audio.speech.create(
+      model="tts-1-hd",
+      voice="onyx",
+      input=text
+    )
+    response.stream_to_file(speech_file_path)
+    return speech_file_path
